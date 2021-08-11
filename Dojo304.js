bf629a6677a4f8966a4f1fbfc3662cdf52e8f219
Ray Lin <rayz.lin90@gmail.com>
2021-08-10 22:08:14 -0400

    daily algo

--- Dojo304.js
+++ Dojo304.js
@@ -1,37 +1,30 @@
-// Given a string, returns whether the sequence of various parentheses, braces and brackets within it are valid. 
+// write a function that given a sorted array of page numbers, return a string representing a book index. Combine consercutive pages to create ranges
 
-// Example 1: "({[({})]})" --> true
-// Example 2: "d(i{a}l[t]o)n{e!" --> false
-// Example 2: "{{[]}}(){}{()}" --> true
+// ex: [1,3,4,5,7,8,10,12] --> "1, 3-5, 7-8, 10, 12"
 
-// hint: consider using an array or object to solve
-
-function bracesValid(str) {
-
-    let stack = [];
-
-    for(let i = 0; i < str.length; i++) {
-        if(str[i] == "(" || str[i] == "{" || str[i] == "[") {
-            stack.push(str[i]);
-        } else if (str[i] == ")" && stack[stack.length-1] == "(") {
-            stack.pop();
-        } else if (str[i] == "}" && stack[stack.length-1] == "{") {
-            stack.pop();
-        } else if (str[i] == "]" && stack[stack.length-1] == "[") {
-            stack.pop();
-        } else if (str[i] == ")" || str[i] == "}" || str[i] == "]") {
-            return false;
+const bookIndex = arr => {
+    // empty variable to hold our output
+    var str = "";
+    // forloop iteration
+    for(let i = 0; i < arr.length; i++) {
+        // compare arr[i] + 1 with arr[i + 1], and if it matches
+        if(arr[i] + 1 == arr[i + 1]) {
+            // we set start as arr[i]
+            let start = arr[i];
+            // we keep looping till the end
+            while(arr[i] + 1 == arr[i + 1]) {
+                i++
+            }
+            // once out of while loop, we set end as arr[i]
+            let end = arr[i];
+            // concat. into out output str
+            str += start + "-" + end + ",";
+        } else {
+            // if arr[i] + 1 with arr[i + 1] does not match, then we just add it to our output
+            str += arr[i] + ",";
         }
     }
-
-    if(stack.length == 0) {
-        return true;
-    } else {
-        return false;
-    }
-
+    return str;
 }
 
-console.log(bracesValid("({[({})]})"))
-console.log(bracesValid("d(i{a}l[t]o)n{e!"))
-console.log(bracesValid("{{[]}}(){}{()}"))
+console.log(bookIndex([1,3,4,5,7,8,10,12]));
