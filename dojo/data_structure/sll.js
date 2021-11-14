12b4e295df99a0b566b9f65c57e58068e55058dc
Ray Lin <rayz.lin90@gmail.com>
2021-11-13 23:54:21 -0500

    ssl removeSecondFromLast & removeNegative

--- dojo/data_structure/sll.js
+++ dojo/data_structure/sll.js
@@ -163,6 +163,42 @@ class SLL {
         // console.log(runner.next.value)
     }
 
+    removeSecondToLast() {
+        if(!this.head) {
+            return this;
+        }
+        if(!this.head.next || !this.head.next.next) {
+            this.removeFromFront();
+            return this;
+        }
+        if(!this.head.next.next) {
+
+        }
+        let runner = this.head;
+        while(runner.next.next.next) {
+            runner = runner.next;
+        }
+        let removedNode = runner.next;
+        runner.next = runner.next.next;
+        removedNode.next = null;
+    }
+// 1, 2, 3
+    removeNegative() {
+        let runner = this.head;
+        while(runner.next) {
+            if(runner.next.value < 0) {
+                let removedNode = runner.next;
+                runner.next = runner.next.next;
+                removedNode.next = null;
+                continue;
+            }
+            runner = runner.next;
+        }
+        if(this.head.value < 0) {
+            this.removeFromFront();
+        }
+    }
+
     contains(value) {
         let runner = this.head;
         while(runner) {
@@ -185,16 +221,17 @@ class SLL {
     }
 }
 
-
-
 let list = new SLL();
-list.addToBack(2);
+list.addToBack(-2);
 list.addToBack(3);
-list.addToBack(11);
-list.addToBack(4);
-list.addToBack(7);
-list.addToBack(1);
-list.addToFront(31);
+list.addToBack(-11);
+// list.addToBack(4);
+// list.addToBack(-7);
+// list.addToBack(-17);
+// list.addToBack(-13);
+// list.addToBack(1);
+list.addToFront(-31);
+// list.addToBack(-5);
 console.log(list.print());
 // list.moveMinToFront();
 // list.moveMaxToBack();
@@ -202,7 +239,9 @@ console.log(list.print());
 // list.removeFromFront();
 // list.appendValue(50,1);
 // list.appendValue(3,100);
-list.prependValue(101,50)
+// list.prependValue(101,50)
 // list.removeFromBack();
 // console.log(list.contains(2));
+// list.removeNegative();
+list.removeSecondToLast();
 console.log(list.print());
