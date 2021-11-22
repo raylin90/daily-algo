e276717793e3ed1f24dc20466aa399aded3b1c15
Ray Lin <rayz.lin90@gmail.com>
2021-11-22 18:40:21 -0500

    review

--- 014_recursion.js
+++ 014_recursion.js
@@ -3,22 +3,18 @@
 
 // write a recursive function that, given a number, return the sum of the integers from one up to that given number
 // ex: given 5 would return 1 + 2 + 3 + 4 + 5 == 15
-// ex: given 2.5 would return 1 + 2 == 2
+// ex: given 2.5 would return 1 + 2 == 3
 
-const recursiveSigma = num => {
-
+const recursiveSum = num => {
     num = Math.floor(num);
-
-    if(num == 1) {
+    // base case:
+    if(num === 1) {
         return 1;
     }
-
-    return num + recursiveSigma(num - 1);
+    return num + recursiveSum(num - 1);
 }
-
-console.log(recursiveSigma(5));
-console.log(recursiveSigma(2.5));
-
+console.log(recursiveSum(5))
+console.log(recursiveSum(2.5))
 
 
 
@@ -26,19 +22,17 @@ console.log(recursiveSigma(2.5));
 // write a recursive function that, given a number, returns the product of integers up to a given number
 // ex: given 4 would return 1 * 2 * 3 * 4 = 24;
 // ex: given 2.5 would return 1 * 2 = 2
-
 const recursiveFactorial = num => {
-
     num = Math.floor(num);
-    
-    if(num == 1) {
+    // base case:
+    if(num === 1) {
         return 1;
     }
-
-    return num * (recursiveFactorial(num - 1));
-
-
+    if(num === 0) {
+        return 0;
+    }
+    return num * recursiveFactorial(num - 1);
 }
-
-console.log(recursiveFactorial(4));
-console.log(recursiveFactorial(2.5));
+console.log(recursiveFactorial(4))
+console.log(recursiveFactorial(2.5))
+console.log(recursiveFactorial(0))
--- 015_recursiveFibonacci.js
+++ 015_recursiveFibonacci.js
@@ -3,15 +3,15 @@
 
 // F0  F1  F2  F3  F4  F5  F6  F7  F8  F9  F10
 //  0   1   1   2   3   5   8  13  21  34  55  
-
 const recursiveFibonacci = num => {
-    if(num == 0) {
+    // base
+    if(num === 0) {
         return 0;
     }
-    if(num == 1) {
+    if(num === 1) {
         return 1;
     }
-    return recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2)
+    return recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2);
 }
-
-console.log(recursiveFibonacci(1)); 
+console.log(recursiveFibonacci(7))
+console.log(recursiveFibonacci(10))
