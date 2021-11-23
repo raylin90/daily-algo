d593c212c67f4a4f6454afc6d8ce5393be7bbd3d
Ray Lin <rayz.lin90@gmail.com>
2021-11-23 18:57:44 -0500

    shortestPallindrom & anagramsCount

---
+++ dojo/016_createShortestPallindrome.js
@@ -0,0 +1,46 @@
+// reviewing the provided in st, create the shortesst possible string by adding characters to the end of st to make it a pallindrome
+// Ex: st = "race" => "racecar"
+// Ex: st = "rada" => "radar"
+// Ex: st = "raco" => "racocat"
+
+const shortestPallindrome = st => {
+    let tempStr = "";
+    let index;
+    // iterate backward from input string
+    for(let i = st.length -1; i >= 0; i--) {
+        // concat. into a new string
+        tempStr += st[i];
+        // check if the new string is a pallindrome or not;
+        if(isPallindrome(tempStr)) {
+            // console.log(tempStr)
+            // if yes, we want to set the start point where the pallindrome begin.
+            index = i;
+        }
+    }
+    // set new variable as input string, so we can append characters into it;
+    let output = st;
+    // so we have the starting point, we want to loop from starting point - 1, till 0, and append to output string
+    for(let j = index - 1; j >= 0; j--) {
+        output += st[j];
+    }
+    return output;
+}
+
+function isPallindrome(inputString) {
+    // for loop iterate the input, half of the length, because we comparing start vs end
+    for(let i = 0; i < inputString.length / 2; i++) {
+        // set j to keep track end element of the input, so we can compare with i (start element)
+        let j = inputString.length - 1 - i;
+        // compare start (i) an end (j), any moment when it's not equal, then we dont have palindrome
+        if(inputString[i] !== inputString[j]) {
+            // return false if not match
+            return false;
+        }
+    }
+    // once finish looping, which means everything matching, then it's true
+    return true;
+}
+
+console.log(shortestPallindrome("race"));   // "racecar"
+console.log(shortestPallindrome("rada"));   // "radar"
+console.log(shortestPallindrome("raco"));   // "racocat"
---
+++ dojo/017_anagramsCount.js
@@ -0,0 +1,27 @@
+// define aa group of anagrams as aa list of wors, where for each pair of words w1 and w2, w1 is an anagram of w2;
+
+
+function anagrams(words){
+    newList = [];
+    // iterate the array;
+    for(let i = 0; i < words.length; i++) {
+        // re-arrange the word alphabetically
+        let alpha = words[i].split("").sort().join("");
+        // once re-arranged, we re-store into the newList;
+        // console.log(alpha)
+        newList.push(alpha);
+    }
+    // sort the new array alphabetically
+    newList = newList.sort();
+    console.log(newList)
+    // set count to count
+    let count = 0;
+    // iterate the newList, and check if j and j+1 is same or not, if yes, then we increase the j till no match, and increase the count same time, so it's unique;
+    for(let j = 0; j < newList.length; j++) {
+        if(newList[j] !== newList[j+1]) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(anagrams(["tea", "eat", "apple", "ate", "vaja", "cut", "java", "utc"])) // 4
