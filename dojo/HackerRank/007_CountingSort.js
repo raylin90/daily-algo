c505718782275826d6f18645a1b432cb511374ec
Ray Lin <rayz.lin90@gmail.com>
2021-11-27 15:15:33 -0500

    countingSort

--- dojo/HackerRank/007_CountingSort.js
+++ dojo/HackerRank/007_CountingSort.js
@@ -1,66 +1,24 @@
-// const coutingSort = arr => {
-//     if(arr.length < 2) return arr;
-//     let maxValue = arr[0];
-//     for(let i = 1; i < arr.length; i++) {
-//         if(arr[i] > maxValue) {
-//             maxValue = arr[i];
-//         }
-//     }
-//     // console.log(maxValue);
-
-//     const countingArray = new Array(maxValue + 1);
-//     for(let value of arr) {
-//         if(!countingArray[value]) {
-//             countingArray[value] = 0;
-//         }
-//         countingArray[value]++;
-//         // console.log(countingArray)
-//     }
-
-//     const resultArray = [];
-//     for(let i = 0; i < countingArray.length; i++) {
-//         while(countingArray[i] > 0) {
-//             resultArray.push(i);
-//             countingArray[i]--;
-//         }
-//     }
-//     return resultArray;
-// }
-// console.log(coutingSort([1,0,3,1,3,1]))
-// console.log(coutingSort([1,1,3,2,1]))
-
-
+const coutingSort = arr => {
+    let counts = Array(100).fill(0);
+    arr.forEach(item => {++counts[item];});
+    return counts;
+}
+console.log(coutingSort([1,0,3,1,3,1]))
 
 
 
 
 const coutingSort = arr => {
-    if(arr.length < 2) return arr;
-    let maxValue = arr[0];
-    for(let i = 1; i < arr.length; i++) {
-        if(arr[i] > maxValue) {
-            maxValue = arr[i];
-        }
+    let n = arr.length;
+    let countingArray = [];
+    for(let i = 0; i < arr.length; i++) {
+        countingArray.push(0)
     }
-    console.log(maxValue);
 
-    const countingArray = new Array(maxValue + 1);
-    // for(let value of arr) {
-    //     if(!countingArray[value]) {
-    //         countingArray[value] = 0;
-    //     }
-    //     countingArray[value]++;
-    //     // console.log(countingArray)
-    // }
-    console.log(countingArray)
-    // const resultArray = [];
-    // for(let i = 0; i < countingArray.length; i++) {
-    //     while(countingArray[i] > 0) {
-    //         resultArray.push(i);
-    //         countingArray[i]--;
-    //     }
-    // }
-    // return resultArray;
+
+    for(let i = 0; i < arr.length; i++) {
+        countingArray[arr[i]]++;
+    }
+    return countingArray;
 }
 console.log(coutingSort([1,0,3,1,3,1]))
-// console.log(coutingSort([1,1,3,2,1]))
