1c7240da147e0b0c39bec2dff58609f56c837091
Ray Lin <rayz.lin90@gmail.com>
2022-01-02 22:58:46 -0500

    daily algo

--- Leetcode/035_EmployeeFreeTime.js
+++ Leetcode/035_EmployeeFreeTime.js
@@ -4,7 +4,7 @@
 we need to find maxEnd, and minStart
 
 
-if start > end, we have gap: maxEnd = [1], minStart = [0]
-if start <= end, it's has same interval
+if start > end, we have gap:  minStart = [0]
+if start <= end, it's has same interval, compare end, maxEnd = max([1])
 
 */
