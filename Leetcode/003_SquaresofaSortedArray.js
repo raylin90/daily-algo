e12de93d7cb0e384f3791fe388d7438d5d4c8e2e
Ray Lin <rayz.lin90@gmail.com>
2022-01-03 12:15:28 -0500

    daily algo

--- Leetcode/003_SquaresofaSortedArray.js
+++ Leetcode/003_SquaresofaSortedArray.js
@@ -34,3 +34,37 @@ var sortedSquares = function(nums) {
 };
 console.log(sortedSquares([-4,-1,0,3,10]));     // [0,1,9,16,100]
 console.log(sortedSquares([-7,-3,2,3,11]));     // [4,9,9,49,121]
+
+
+
+
+
+var sortedSquares = function(nums) {
+    // new array to hold output
+    let output = [];
+    let newIdx = nums.length - 1;
+    // two pointer to track of start and end element
+    let start = 0;
+    let end = nums.length - 1;
+    // we need <= because if we only do <, we will missing [0] element as empty
+    while(start <= end) {
+        // square both element
+        let squaredStart = Math.pow(nums[start], 2);
+        let squaredEnd = Math.pow(nums[end], 2);
+        // console.log(squaredStart, squaredEnd)
+
+        // find the largest number after squre, and set the last index of output to that largest number
+        if(squaredStart >= squaredEnd) {
+            output[newIdx] = squaredStart;
+            start++;
+        } else {
+            output[newIdx] = squaredEnd;
+            end--;
+        }
+        newIdx--;
+    }
+    return output;
+};
+
+console.log(sortedSquares([-4,-1,0,3,10]));     // [0,1,9,16,100]
+console.log(sortedSquares([-7,-3,2,3,11]));     // [4,9,9,49,121]
