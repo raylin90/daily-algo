28d2b35809894c20c11fc24f76306e15007d6bca
Ray Lin <rayz.lin90@gmail.com>
2022-01-18 23:13:24 -0500

    daily algo

--- Leetcode/056_DesignHashMap.js
+++ Leetcode/056_DesignHashMap.js
@@ -50,30 +50,100 @@ MyHashMap.prototype.remove = function(key) {
  */
 
 
+const hash = function(string, size) {
+    string = string.toString();
+    // set initial idx = 0
+    let idx = 0;
+    // for loop the string, and add up each corresponding code
+    for(let i = 0; i < string.length; i++) {
+        idx += string.charCodeAt(i);
+    }
+    // our end idx will be large, so we modulo to it's map size to get smaller index
+    return idx % size;
+}
 
 class MyHashMap {
     constructor() {
-        this.obj = new Array(100000).fill(-1);;
+        this.map = new Array(3);
     }
     
     put(key, value) {
-        this.obj[key] = value;
+        // get hashed index from helper function
+        let index = hash(key, this.map.length);
+        // console.log(index);
+        // console.log("map", this.map[index])
+        // console.log("index", index)
+
+        // if no element at hashed index, we set the key, value as nesterd array
+        if(this.map[index] === undefined) {
+            this.map[index] = [
+                [key, value]
+            ]
+            // else we might have 1 or more element, we what to parse through (THE HASHED INDEX) and see if key:value already existed or not
+        } else {
+            let isInserted = false;
+            for(let i = 0; i < this.map[index].length; i++) {
+                // if we found matching key, override the value
+                if(this.map[index][i][0] == key) {
+                    this.map[index][i][1] = value;
+                    isInserted = true;
+                }
+            }
+            // once parse is done, and we cannot find the key, then push a new array into the map array
+            if(isInserted === false) {
+                this.map[index].push([key, value]);
+            }
+        }
     }
     
     get(key, value) {
-        return this.obj[key] >= 0 ? this.obj[key] : -1
+        // get hashed index from helper function
+        let index = hash(key, this.map.length);
+
+
+        // if hashed index is false, no need to perform anything
+        if(!this.map[index]) {
+            return -1;
+        } else {
+            // console.log(this.map[index])
+            // parse throught the hashed index array, find the key, and delete from the list
+            for(let i = 0; i < this.map[index].length; i++) {
+
+                if(this.map[index][i][0] == key) {
+                    return this.map[index][i][1];
+                    // console.log(this.map[index][i][1])
+                }
+            }
+
+        }
+        return -1;
     }
     
     remove(key, value) {
-        return this.obj[key] = -1;
+        // get hashed index from helper function
+        let index = hash(key, this.map.length);
+
+        // if hashed index is false, no need to perform anything
+        if(!this.map[index]) {
+            return;
+        } else {
+            // parse throught the hashed index array, find the key, and delete from the list
+            for(let i = 0; i < this.map[index].length; i++) {
+                if(this.map[index][i][0] == key) {
+                    return this.map[index].splice(i,1)
+                }
+            }
+        }
     }
 }
+
 let map = new MyHashMap();
 map.put(1,1);
 map.put(2,2);
-map.put(2,5);
-map.put(11,0);
-console.log(map.get(11));
-map.remove(2,2)
-console.log(map.obj)
-
+console.log(map.map)
+map.put(2,1)
+console.log(map.get(2));
+// map.get(6);
+// map.get(7);
+// map.remove(1);
+console.log(map.map)
