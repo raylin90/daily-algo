45289c8393007d23c8395c0a01d659136c6c6130
Ray Lin <65415822+raylin90@users.noreply.github.com>
2022-01-27 21:11:59 -0500

    daily algo

--- Leetcode/068_LongestSubstringWithoutRepeatingCharacters.js
+++ Leetcode/068_LongestSubstringWithoutRepeatingCharacters.js
@@ -72,3 +72,22 @@ var lengthOfLongestSubstring = function(s) {
 // console.log(lengthOfLongestSubstring("pwwkew"));
 // console.log(lengthOfLongestSubstring("aab"));
 console.log(lengthOfLongestSubstring("dvdf"));
+
+
+
+
+var lengthOfLongestSubstring = function(s) {
+    let l = 0, r = 0, max = 0
+    let set = new Set()
+    
+    while(r < s.length) {
+        if (set.has(s.charAt(r))) {
+            set.delete(s.charAt(l++)) // Important! explained below
+        } else {
+            set.add(s.charAt(r++))
+            max = Math.max(max, set.size)
+        }
+    }
+    
+    return max
+};
