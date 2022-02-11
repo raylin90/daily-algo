df0cb33ca5ad300a119f6cd2274e791fa95062c5
Ray Lin <65415822+raylin90@users.noreply.github.com>
2022-02-10 20:41:51 -0500

    daily algo

--- Leetcode/075_review.js
+++ Leetcode/075_review.js
@@ -1,24 +1,24 @@
+const sort = (nums1, m, nums2, n) => {
+    let lastIdx = m + n - 1;
 
-
-const maxOnes = (arr) => {
-    let count = 1;
-    let maxCount = 0;
-
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] === 1) {
-            count++;
+    while(nums2.length) {
+        // console.log(nums2[n-1], nums1[m-1])
+        if(nums1[m-1] > nums2[n-1]) {
+            nums1[lastIdx] = nums1[m-1];
+            lastIdx--;
+            m--;
         } else {
-            maxCount = Math.max(maxCount, count);
-            count = 0;
+
+
+            nums1[lastIdx] = nums2.pop();
+            n--;
+            lastIdx--;
         }
+
     }
+    console.log(nums1);
 
-    return maxCount;
 }
-console.log(maxOnes([1,1,0,1,1,1]));
-// for loop (i)
-// i and i+1
-    // count++
-
-    // re-set maxCount
-    // count = 1
+// console.log(sort([1,2,3,0,0,0],3,[2,5,6],3));
+console.log(sort([1],1,[],0));
+console.log(sort([0],0,[1],1));
