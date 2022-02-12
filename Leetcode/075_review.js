1a7e6451d1b376d02de14db8b48a9d1870ada76d
Ray Lin <65415822+raylin90@users.noreply.github.com>
2022-02-11 21:46:52 -0500

    daily algo

--- Leetcode/075_review.js
+++ Leetcode/075_review.js
@@ -1,24 +1,11 @@
-const sort = (nums1, m, nums2, n) => {
-    let lastIdx = m + n - 1;
+const removeElement = (nums, k) => {
 
-    while(nums2.length) {
-        // console.log(nums2[n-1], nums1[m-1])
-        if(nums1[m-1] > nums2[n-1]) {
-            nums1[lastIdx] = nums1[m-1];
-            lastIdx--;
-            m--;
-        } else {
-
-
-            nums1[lastIdx] = nums2.pop();
-            n--;
-            lastIdx--;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === k) {
+            nums.splice(i, 1);
+            i--;
         }
-
     }
-    console.log(nums1);
-
+    return nums;
 }
-// console.log(sort([1,2,3,0,0,0],3,[2,5,6],3));
-console.log(sort([1],1,[],0));
-console.log(sort([0],0,[1],1));
+console.log(removeElement([3,2,2,3],2));
