a556bc00c397eab4fbb08e5bb5f9cf0f7294a0bd
Ray Lin <65415822+raylin90@users.noreply.github.com>
2022-02-15 22:24:51 -0500

    daily algo

--- Leetcode/075_review.js
+++ Leetcode/075_review.js
@@ -4,25 +4,28 @@ input: arr;
 output: boolean
  */
 
-function solution(arr) {
+function solution(nums) {
 
-    let start = 0;
-    let end = arr.length - 1;
+    if(nums.length < 3) return Math.max(...nums);
 
-    while(start < end) {
-        if(arr[start] === 0) {
-            arr.splice(start, 1);
-            arr.push(0);
-            end --;
-        } else {
-            start ++;
+    let firstMax = 0, secondMax = 0, thirdMax = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > firstMax) {
+            thirdMax = secondMax;
+            secondMax = firstMax;
+            firstMax = nums[i];
+        } else if(nums[i] > secondMax && nums[i] !== firstMax) {
+            thirdMax = secondMax;
+            secondMax = nums[i];
+        } else if(nums[i] > thirdMax && nums[i] !== secondMax && nums[i] !== firstMax) {
+            thirdMax = nums[i];
         }
+        // console.log(firstMax, secondMax, thirdMax);
     }
-    return arr;
+
+    return thirdMax >= 0 ? thirdMax : firstMax;
 }
 
-console.log(solution([0,1,0,0,0,0,3,12]));
-// console.log(solution([0]));
-
-
-// test main
+console.log(solution([2,2,3,1]));        // 1
+console.log(solution([1,2,2,2,2,5,3,5]));      // 2
