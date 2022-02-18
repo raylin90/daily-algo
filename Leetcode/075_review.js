618fd43b73779b0750f8a694e66465edad2ca3de
Ray Lin <65415822+raylin90@users.noreply.github.com>
2022-02-17 22:50:53 -0500

    daily algo

--- Leetcode/075_review.js
+++ Leetcode/075_review.js
@@ -371,3 +371,238 @@ function solution(str1) {
 }
 console.log(solution([1,3,4,2,2]));     // [1,2]
 console.log(solution([3,1,3,4,2]));     // [1,2]
+
+
+
+
+
+function solution(str1) {
+    
+    let map = new Map();
+
+    for(let i = 0; i < str1.length; i++) {
+        for(let j = 0; j < str1.length; j++) {
+            // console.log(str1[i][j])
+            let key = i + j;
+            let val = str1[i][j];
+
+            if(!map.has(key)) {
+                map.set(key, [val]);
+            } else {
+                map.set(key, [...map.get(key), val]);
+            }
+        }
+    }
+    // console.log(map);
+    let result = [];
+
+    for(const [key, val] of map) {
+        // console.log(key, val)
+        if(key % 2 === 0) {
+            result.push(...val.reverse());
+        } else {
+            result.push(...val)
+        }
+    }
+    return result;
+}
+console.log(solution([[1,2,3],[4,5,6],[7,8,9]]));
+
+
+
+
+
+
+
+function solution(str1) {
+    
+
+    let rows = str1.length;
+    let cols = str1[0].length;
+
+let result = new Array(rows+cols-1).fill(null).map(()=> []);
+
+    for(let i = 0; i < str1.length; i++) {
+        for(let j = 0; j < str1.length; j++) {
+            // console.log(str1[i][j])
+            let key = i + j;
+            let val = str1[i][j];
+
+            if(key % 2 === 0) {
+                result[key].unshift(val);
+            } else {
+                result[key].push(val);
+            }
+
+        }
+    }
+    console.log(result.flat());
+}
+console.log(solution([[1,2,3],[4,5,6],[7,8,9]]));
+
+
+
+
+
+
+function solution(matrix) {
+
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+
+    let result = [];
+
+    console.log(top, right, bottom, left);
+
+    while(top <= bottom && left <= right) {
+
+        for(let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+        for(let j = top; j <= bottom; j++) {
+            result.push(matrix[j][right])
+        }
+        right--;
+
+        if(top < bottom) {
+            for(let x = right; x >= left; x--) {
+                result.push(matrix[bottom][x]);
+            }
+            bottom--;
+        }
+
+        if(left < right) {
+            for(let y = bottom; y >= top; y--) {
+                result.push(matrix[y][left]);
+            }
+            left++;
+        }
+
+        // console.log(result);
+    }
+    return result;
+}
+console.log(solution([[1,2,3,4],[5,6,7,8],[9,10,11,12]]));
+
+
+
+
+function soluction(nums, k) {
+    let i = 0, j = 0;
+    let sum = 0;
+    let minLength = Infinity;
+    let count = 0;
+
+    while(j < nums.length) {
+        sum += nums[j];
+        count++;
+        j++;
+        while(sum >= k) {
+            minLength = Math.min(count, minLength);
+            sum -= nums[i];
+            i++;
+            count--;
+        }
+    }
+    console.log(minLength);
+}
+console.log(soluction([2,3,1,2,4,3], 7))
+
+
+
+
+
+function soluction(nums, k) {
+    let i = 0, j = 0;
+    let sum = 0;
+    let minLength = Infinity;
+    let count = 0;
+
+    while(j < nums.length) {
+
+        sum+=nums[j];
+
+        while(sum >= k) {
+            minLength = Math.min(minLength, j-i+1);
+            sum -= nums[i];
+            i++;
+        }
+
+        j++;
+        console.log(minLength);
+    }
+}
+console.log(soluction([1,4,4], 4))
+
+
+
+
+function soluction(nums, k) {
+
+    k = k % nums.length;
+    if(k === 0) return nums;
+
+    let resut = [];
+
+    for(let i = nums.length - k; i < nums.length; i++) {
+        resut.push(nums[i]);
+    }
+
+    for(let i = 0; i <= k; i++) {
+        resut.push(nums[i]);
+    }
+
+    return resut;
+}
+console.log(soluction([1,2,3,4,5,6,7], 10))
+
+
+
+
+
+function soluction(nums, k) {
+
+    k = k % nums.length;
+    if(k === 0) return nums;
+
+    let arr1 = nums.slice(nums.length - k);
+    let arr2 = nums.slice(0, k+1);
+    // console.log(arr1, arr2);
+    return [...arr1, ...arr2]
+
+}
+console.log(soluction([1,2,3,4,5,6,7], 10))
+
+
+
+
+function soluction(nums, k) {
+
+    k = k % nums.length;
+    if(k === 0) return nums;
+
+    for(let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
+    return nums;
+}
+console.log(soluction([1,2,3,4,5,6,7], 10))
+
+
+
+
+
+
+function soluction(nums) {
+
+    nums = nums.split(" ");
+    // console.log(nums)
+
+    const result = nums.filter(word => word.length > 0)
+    console.log(result.reverse());
+}
+console.log(soluction("  hello world  "))
